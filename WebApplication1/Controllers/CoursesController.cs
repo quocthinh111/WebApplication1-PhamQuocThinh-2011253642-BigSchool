b9f096670ba5b74c8834c0a7eb4b18ab7847c241
Administrator <Administrator@DESKTOP-THINH>
2023-03-13 06:32:59 +0700

    Add project files.

---
+++ WebApplication1/Controllers/CoursesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Models.Viewmodel;
+
+
+namespace WebApplication1.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+            
+        }
+       
+    
+    }
+}
---
+++ WebApplication1/Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Course
+    {
+       public int Id { get; set; }
+       [Required]
+       public string LectureId { get; set; }
+       [Required]
+       [StringLength(255)]
+       public string Place { get; set; }
+       public DateTime Datetime { get; set; }
+       public Category Category { get; set; }
+        [Required]
+        public byte CategoryId { get; set; }
+    }
+  
+}
